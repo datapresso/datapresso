082dad8c28caee942f3f1678e4961816be659ae5
Fernando <frogzta@gmail.com>
2013-06-19 14:41:40 -0500

    First upload

---
+++ visualization/incident.js
@@ -0,0 +1,8 @@
+var incident = [{name:"Smuggle01", state:"az", loc:"Nogales Port", cords:[[[31.3403,-110.9336]]]},
+				 {name:"Smuggle02", state:"az", loc:"San Luis", cords:[[[32.4869,-114.7814]]]},
+				 {name:"Arrest01", state:"az", loc:"Tucson", cords:[[[32.2217,-110.9258]]]},
+				 {name:"Bomb01", state:"ga", loc:"Columbus", cords:[[[32.4608,-84.9878]]]},
+				 {name:"Bomb02", state:"mi", loc:"Holly", cords:[[[42.7919,-83.6278]]]},
+				 {name:"MissingPerson01", state:"sd", loc:"South Dakota", cords:[[[44.2961,-99.2367]]]},
+				 {name:"Biological01", state:"ma", loc:"Cambridge", cords:[[[42.2225,-71.0638]]]}
+				 ];
