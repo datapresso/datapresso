dd9e72c2d40e44a120328de79e0ee66724a093be
Fernando <frogzta@gmail.com>
2013-06-27 10:37:50 -0500

    Test

---
+++ visualization/map/extracted.js
@@ -0,0 +1,25 @@
+var locs =["Dublin Airport","The flight","The flight","Dana Air flight","The flight","Enugu Airport","Port Harcourt Airport","BFD","Abuja International Airport","International Airport","International Airport","Jet Airways flight","Kennedy Airport","Airways flight","Philadelphia Airport","Heathrow Airport","Australian airports","Sydney Airport","Perth Airport","Austin airport","Bergstrom International Airport","Kansai Airport","Gimpo Airport","Ciampino Airport","The airport","Ben Gurion International Airport","Chaudhary Charan Singh International Airport","Shanghai Pudong International Airport","Teterboro Airport","Teterboro Airport","Dulles International Airport","Richland Township airport","Cambria County Airport","Dulles International Airport","Richland Township airport","Cambria County Airport","London Heathrow airport","Logan Airport",]
+
+//var locs = ["Houston","Austin","Alabama",""]
+//var locs =[
+//"Lebanon","Lebanon","Damascus","Tripoli","Syria","Damascus",
+//"Lebanon","Syria","New Delhi","China","Nepal","Russia","Milan","Dublin",
+//"Dublin","Lagos","Nigeria","Indonesia","Iran","Libya","Burkina Faso","Kano",
+//"Nigeria","Port Harcourt","Abuja","Nigeria","Bellview","Nigeria","Nigeria","Delhi",
+//"Dubai","Bangkok","Delhi","Asia","Asia","India","Sri Lanka","Indonesia","United Arab Emirates",
+//"Wales","Miami","New York","Miami","New York","United States","Dubai","Bangkok",
+//"Dubai","Germany","United States","Mexico","United States","United States","Mexico",
+//"Tucson","United States","Mexico","Mexico","United States","United States",
+//"United States","United States","United States","Frontera","Guatemala","United States",
+//"United States","United States","Los Angeles Quinta","United States","United States",
+//"United States","Arizona","Mexico","Tucson","United States","Mexico","Tucson","United States",
+//"El Paso","Guanajuato","Tucson","Mexico City","United States","Arizona","United States",
+//"Homeland","Mexico","United States","Mexico","United States","Arizona","Tucson","Mexico City",
+//"Tucson","Del Rio","San Diego","Calexico","United States","Nogales","Mexico","Tucson",
+//"Arizona","Aguascalientes","Prescott Ariz","Mexico City","Nogales","Atlanta","Mexico",
+//"United States","Missouri","Street Louis","United States","United States","Massachusetts",
+//"Cape","Chatham","Yarmouth","New","Santa Teresa","United States","Mexico","United States",
+//"El Paso","New","El Paso","Mexico","United States","United States","United States",
+//"Arizona","Nogales","United States","Nogales","Nogales","Sonora","Mexico","Nogales",
+//"Sonora","Mexico","Homeland","Yonkers","New","New","Newark","Swampscott","Boston"
+//];
