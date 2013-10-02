402469da4a4c70781978b98708faf6817b6b63fa
datapresso <cmding1@comkraft.com>
2013-10-01 23:30:22 -0400

    data dump with additional test data import result

--- datapresso/desc/datapresso.sql
+++ datapresso/desc/datapresso.sql
@@ -1,6 +1,14 @@
 -- remove all data
+use datapresso;
 delete from address;
 delete from person;
 delete from document;
 delete from entity;
 delete from organization;
+delete from event;
+
+
+
+--
+select * from datapresso.event
+select * from datapresso.document
